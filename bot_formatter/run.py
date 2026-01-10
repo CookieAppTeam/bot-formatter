bddc2a14bb9f49e428c5fe30f2aff3f8cdfbfe43
Timo <35654063+tibue99@users.noreply.github.com>
2026-01-10 17:03:30 +0100

    Check missing language file keys

--- bot_formatter/run.py
+++ bot_formatter/run.py
@@ -4,6 +4,7 @@ import argparse
 from pathlib import Path
 
 from libcst import codemod
+import yaml
 
 from bot_formatter.formatters import DPY, EZCORD, PYCORD, LANG
 
@@ -11,6 +12,7 @@ from bot_formatter.formatters import DPY, EZCORD, PYCORD, LANG
 class Output:
     modified_files: list[str] = []
     failed_files: list[str] = []
+    failed_checks: dict[str, list[str]] = {}
 
     def __init__(self, config: argparse.Namespace):
         self.config = config
@@ -20,6 +22,9 @@ class Output:
 
     def error(self, file: str, error: Exception):
         self.failed_files.append(f"{file}: {error}")
+
+    def check_failed(self, file: str, error_txt: str):
+        self.failed_checks[file] = error_txt
 
     @staticmethod
     def _check_plural(word: str, count: int) -> str:
@@ -46,6 +51,10 @@ class Output:
             report += f"\n\n{self._check_plural('error', len(self.failed_files))} occurred"
             report += "\n" + "\n".join(self.failed_files)
 
+        for file, errors in self.failed_checks.items():
+            report += f"\n\n\n------ CHECKS FAILED IN {file.upper()} ------"
+            report += "\n\n" + "\n".join(errors)
+
         print(report)
 
 
@@ -61,7 +70,8 @@ class BotFormatter:
             "--lib", default="pycord", choices=["dpy", "pycord"], help="The library to use."
         )
         parser.add_argument("--ezcord", action="store_true", help="Use Ezcord formatters.")
-        parser.add_argument("--lang", help="The language directory to format.")
+        parser.add_argument("--no-yaml", action="store_true", help="Disable YAML formatters.")
+        parser.add_argument("--lang", help="The language directory to check.")
 
         self.config = parser.parse_args(args)
         self.report = Output(self.config)
@@ -77,15 +87,67 @@ class BotFormatter:
         else:
             self.lang_dir = None
 
+        self.check_lang_files()
+
         for file in self.config.files:
             self.format_file(file)
 
         self.report.print_output()
 
+        if len(self.report.failed_checks) > 0:
+            raise SystemExit(1)
+
     def log(self, message: str):
         """Prints a message to the console if the silent mode isn't enabled."""
+
         if not self.config.silent:
             print(message)
+
+    def check_lang_files(self):
+        """Run language file checks to ensure consistency across all language files."""
+
+        if not self.lang_dir:
+            return
+
+        lang_files = list(self.lang_dir.glob("*.yaml")) + list(self.lang_dir.glob("*.yml"))
+
+        lang_contents = {}
+        for file_path in lang_files:
+            with open(file_path, encoding="utf-8") as f:
+                content = yaml.safe_load(f)
+                lang_contents[file_path.name] = content
+
+        def collect_keys(lang_content: dict, parent_key: str | None = None) -> set[str]:
+            """Recursively collects all keys in a nested dictionary."""
+
+            lang_keys = set()
+            for key, value in lang_content.items():
+                full_key = f"{parent_key}.{key}" if parent_key else key
+                lang_keys.add(full_key)
+                if isinstance(value, dict):
+                    lang_keys.update(collect_keys(value, full_key))
+
+            return lang_keys
+
+        # detect differences in lang_contents
+        for file_name, content in lang_contents.items():
+            errors = []
+            for other_file_name, other_content in lang_contents.items():
+                if file_name == other_file_name:
+                    continue
+
+                keys = collect_keys(content)
+                other_keys = collect_keys(other_content)
+
+                missing_keys = other_keys - keys
+
+                if missing_keys:
+                    missing = '\n'.join(sorted([f"- {key}" for key in missing_keys]))
+                    errors.append(f"Missing keys compared to {other_file_name}:\n{missing}")
+
+            if errors:
+                self.report.check_failed(file_name, errors)
+
 
     def format_file(self, filename: str):
         """Runs all enabled formatters on a given file."""
@@ -125,10 +187,7 @@ class BotFormatter:
                 self.report.error(filename, result.error)
 
         # Run language formatters
-        if ext not in ["yaml", "yml"]:
-            return
-
-        if self.lang_dir and self.lang_dir not in Path(filename).parents:
+        if self.config.no_yaml or ext not in ["yaml", "yml"]:
             return
 
         for lang_formatter in LANG:
